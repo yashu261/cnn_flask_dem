a96ce30d72c919e97454e4c7f62b63e5441b1952
yashu261 <yashu2615k@gmail.com>
2026-01-31 15:39:16 +0530

    update

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+    
+@app.route("/<name>")
+def name(name):
+    return render_template("name.html",name=name,image=name+".jpg")
+
+@app.route("/abc")
+def abc():
+    return render_template("abc.html")
+if __name__ == "__main__":
+    app.run(debug=True) 
